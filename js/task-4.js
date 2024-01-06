6283584dd164b5c2fa5d9044eaecb1068ccfb482
Volodymyr Stelmakh <volodymyr.kai@gmail.com>
2024-01-06 17:36:18 +0200

    alltask done

--- js/task-4.js
+++ js/task-4.js
@@ -1,18 +1,21 @@
 const form = document.querySelector('.login-form');
-const email = document.querySelector('.login-form').elements["email"];
-const password = document.querySelector('.login-form').elements["password"];
 
 form.addEventListener('submit', onFormSubmit);
 
-function onFormSubmit(event) {
-    event.preventDefault();
-    if (email.value.length > 1 && password.value.length > 1) {
-        console.log({
-            name: email.value.trim(),
-            password: password.value.trim(),
-        });
-        form.reset();
+function onFormSubmit(e) {
+    e.preventDefault();
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
+
+    const userData = {
+        email,
+        password,
+    };
+
+    if (email.length > 1 && password.length > 1) {
+        console.log(userData);
+        e.target.reset();
     } else {
         alert('All form fields must be filled in');
-    }
+    };
 };
