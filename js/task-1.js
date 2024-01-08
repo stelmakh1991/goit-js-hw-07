404d633f55ab6ba27b8fdfe2f504ad1ba3c26cf2
Volodymyr Stelmakh <volodymyr.kai@gmail.com>
2024-01-08 11:01:16 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,7 @@
 const categories = document.querySelectorAll('.item');
 console.log(`Number of categories: ${categories.length}`);
 // ============================
-Array.from(categories).forEach((category) => {console.log(`Category: ${category.firstElementChild.textContent}`), console.log(`Elements: ${category.lastElementChild.children.length}`)});
+categories.forEach((category) => {
+    console.log(`Category: ${category.firstElementChild.textContent}`);
+    console.log(`Elements: ${category.lastElementChild.children.length}`);
+});
